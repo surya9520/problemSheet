6bde6a18f16fe1edd141c8980266287dfcbd36f8
surya <suryajoshi617@gmail.com>
2024-12-10 12:52:05 +0530

    messages

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,11 @@ import { connectMongoDb } from './src/config/db.js'
 
 connectMongoDb('mongodb://127.0.0.1:27017/DSA?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.1')
 
-app.get('/',(req,res)=>{
-    res.status(200).json({message:"welcome to dsasheet"})
-})
+app.get('/', (req, res) => {
+    console.log('GET request to /');
+    res.status(200).json({ message: "welcome to dsasheet" });
+});
+
 app.listen(4000,()=>{
     console.log("server start at port 4000")
 })
